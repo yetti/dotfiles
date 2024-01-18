00a2cafc3bf9878e326f9d81762d10d74d06878a
Yetrina Battad <12688+yetti@users.noreply.github.com>
2024-01-18 11:20:18 +1100

    Update setup.sh
    comment out zinit

--- setup.sh
+++ setup.sh
@@ -24,9 +24,9 @@ echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
 
 # setup zinit and link .zshrc to home directory
-sh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-mv ~/.zshrc ~/.zshrc_orig
-cp ./.zshrc ~
+# sh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+# mv ~/.zshrc ~/.zshrc_orig
+# cp ./.zshrc ~
 # source ~/.zshrc
 
 # link .gitconfig to home directory
