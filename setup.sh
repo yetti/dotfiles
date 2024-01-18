075965a0f5347594f8c3aedb898b42190baac2f7
Yetrina Battad <12688+yetti@users.noreply.github.com>
2024-01-18 20:02:37 +1100

    Update setup.sh
    remove installation of extra libs, starship installation, etc.

--- setup.sh
+++ setup.sh
@@ -1,31 +1,5 @@
 #!/bin/bash
 
-# install extra OS packages
-apt-get update && export DEBIAN_FRONTEND=noninteractive \
-    && apt-get -y install --no-install-recommends \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    dirmngr \
-    gnupg \
-    lsb-release \
-    software-properties-common \
-    direnv \
-    fzy
-
-# set shell
-chsh -s $(which zsh)
-
-# setup commitizen
-npm install -g yarn pnpm commitizen cz-conventional-changelog
-echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
-
-# install Starship prompt
-curl -sS https://starship.rs/install.sh | sh -s -- --yes
-
-# setup zinit and link .zshrc to home directory
-# sh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-# mv ~/.zshrc ~/.zshrc_orig
 cp ./.zshrc ~
 source ~/.zshrc
 
