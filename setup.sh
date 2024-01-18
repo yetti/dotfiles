d1395f1fd1f516b53bbaa8f487d4cf029fd908c7
Yetrina Battad <12688+yetti@users.noreply.github.com>
2024-01-18 20:26:38 +1100

    Update setup.sh
    remove zinit install

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 chsh -s $(which zsh)
 
-sh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-
 # cp ./.zshrc ~
 # source ~/.zshrc
 
