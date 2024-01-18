1924ad2b0162f131af911b06cbfd4e63e83fa5da
Yetrina Battad <12688+yetti@users.noreply.github.com>
2024-01-18 21:04:40 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,3 @@ chsh -s $(which zsh)
 
 mv ~/.zshrc ~/.zshrc_orig
 cp ./.zshrc ~
-
-# copy .wakatime.cfg to home directory and set API KEY
-cp ./.wakatime.cfg ~
-sed -i "s/WAKATIME_KEY/$WAKATIME_KEY/g" ~/.wakatime.cfg
