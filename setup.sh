59308de56139f1d7bb3aafd1d394dcae179cfb1a
Yetrina Battad <12688+yetti@users.noreply.github.com>
2024-01-18 11:07:32 +1100

    Update setup.sh
    backup .zshrc before copying from dotfiles

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ curl -sS https://starship.rs/install.sh | sh -s -- --yes
 
 # setup zinit and link .zshrc to home directory
 sh -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-#mv ~/.zshrc ~/.zshrc_orig
+mv ~/.zshrc ~/.zshrc_orig
 cp ./.zshrc ~
 # source ~/.zshrc
 
