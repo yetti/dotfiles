43a56147977f9b029967aa9f8e18c5ef39eb04a2
Yetrina Battad <12688+yetti@users.noreply.github.com>
2024-01-18 20:12:01 +1100

    Update setup.sh
    don't copy .gitconfig

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 cp ./.zshrc ~
-source ~/.zshrc
 
-# link .gitconfig to home directory
-cp ./.gitconfig ~
-
-# link .wakatime.cfg to home directory and set API KEY
+# copy .wakatime.cfg to home directory and set API KEY
 cp ./.wakatime.cfg ~
 sed -i "s/WAKATIME_KEY/$WAKATIME_KEY/g" ~/.wakatime.cfg
