3c8d265ef6a28133bede0b66ffb3da08e7ae5745
Yetrina Battad <12688+yetti@users.noreply.github.com>
2024-10-31 14:10:27 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/bash
 
 # Setup locales
-echo "locales locales/default_environment_locale select en_US.UTF-8" | debconf-set-selections
-echo "locales locales/locales_to_be_generated multiselect en_US.UTF-8 UTF-8" | debconf-set-selections
-sudo rm "/etc/locale.gen"
-sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure locales
+echo "en_AU.UTF-8" | sudo tee -a /etc/locale.gen
+sudo locale-gen
 
 sudo apt-get update
 sudo apt-get upgrade -y
