b4966645b2b54d5f5d244ec51d64959cd2012d6d
Yetrina Battad <12688+yetti@users.noreply.github.com>
2024-10-31 12:52:18 +1100

    Update setup.sh
    Setup locales

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
+# Setup locales
+export LANG=en_AU.UTF-8
+export LANGUAGE=en_AU:en
+export LC_ALL=en_AU.UTF-8
+sudo dpkg-reconfigure locales -f noninteractive
+
 sudo apt-get update
 sudo apt-get upgrade -y
 
